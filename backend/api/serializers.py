0a16603e15656d731725cd1a893cad8871dac95c
Jingying He <jingyingh@student.unimelb.edu.au>
2021-10-26 01:30:55 +1100

    token

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from rest_framework.authtoken.models import Token
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,6 +11,11 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
     def validate_username(self, value):
         try:
