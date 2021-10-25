96928d0c08463d9711454f12f2ee35a6b8a527f3
Jingying He <jingyingh@student.unimelb.edu.au>
2021-10-25 11:52:25 +1100

    serilizer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import UserAccountSerializer
 
 router = routers.DefaultRouter()
+router.register('users', UserAccountSerializer)
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import UserAccount
+from .serializers import UserAccountSerializer
 
-# Create your views here.
+
+class UserAccount(viewsets.UserAccount):
+    queryset = UserAccount.objects.all()
+    serilizer_class = (UserAccountSerializer, )
