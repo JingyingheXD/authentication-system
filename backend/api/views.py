db9e86c335a7a43e7258b08f8fb3b7556cbb257a
Jingying He <jingyingh@student.unimelb.edu.au>
2021-10-25 16:27:39 +1100

    reponse for user input

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import UserAccount
 from .serializers import UserAccountSerializer
 
@@ -7,3 +8,35 @@ from .serializers import UserAccountSerializer
 class UserAccountSet(viewsets.ModelViewSet):
     queryset = UserAccount.objects.all()
     serializer_class = UserAccountSerializer
+
+    # TODO restrict password format and content
+    @action(detail=False, methods=['POST'])
+    def create_account(self, request, pk=None):
+        # Incomplete input
+        if 'email' not in request.data and 'password' in request.data:
+            response = {'message': 'You need to provide an email'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        elif 'password' not in request.data and 'email' in request.data:
+            response = {'message': 'You need to set a password'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        elif 'email' not in request.data and 'password' not in request.data:
+            response = {
+                'message': 'You need to provdie an email and set a password'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+        # Complete input
+        else:
+            email = request.data['email']
+            # Don't allow repeat emails
+            if UserAccount.objects.filter(email=email).exists():
+                response = {'message': 'Email exists'}
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+            else:
+                password = request.data['password']
+                userAccount = UserAccount.objects.create(
+                    email=email, password=password)
+                serializer = UserAccountSerializer(userAccount, many=False)
+                response = {'message': 'Account created',
+                            'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
