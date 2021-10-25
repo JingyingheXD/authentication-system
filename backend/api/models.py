d102b3a728bf909d80bffaa2ae5f25eefd17c1e6
Jingying He <jingyingh@student.unimelb.edu.au>
2021-10-25 11:47:13 +1100

    init models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserAccount(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=15)
+
+     
