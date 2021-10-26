3b0e999781a4a221b681408ad5e21ae08c13dab1
Jingying He <jingyingh@student.unimelb.edu.au>
2021-10-26 22:09:30 +1100

    SignIn test

--- backend/api/tests/test_signin.py
+++ backend/api/tests/test_signin.py
@@ -10,72 +10,142 @@ from ..serializers import UserSerializer
 client = Client()
 
 
-class SignUpTest(TestCase):
+class SignInTest(TestCase):
     """ Test module for adding a user API  """
 
     def setUp(self):
-        self.valid_user = {
+        self.user_previous = {
+            'username': 'hjy@gmail.com',
+            'password': 'hejingying123',
+        }
+
+        self.valid_signin_user = {
             'username': 'hjy@gmail.com',
             'password': 'hejingying123',
         }
 
         """ blank """
-        self.invalid_user_no_username = {
+        self.invalid_signin_user_no_username = {
             'username': '',
             'password': 'hejingying123',
         }
-        self.invalid_user_no_password = {
-            'username': 'hjy_new@gmail.com',
-            'password': '',
-        }
-        self.invalid_user_no_username_password = {
-            'username': '',
+        self.invalid_signin_user_no_password = {
+            'username': 'hjy@gmail.com',
             'password': '',
         }
 
-    def test_add_valid_user(self):
-        response = client.post(
+        """ not blank """
+        self.invalid_signin_nonexist_username = {
+            'username': 'hjy123@gmail.com',
+            'password': 'hejingying123',
+        }
+        self.invalid_signin_invalid_username = {
+            'username': 'hjygmail.com',
+            'password': 'hejingying123',
+        }
+        self.invalid_signin_wrong_password = {
+            'username': 'hjy@gmail.com',
+            'password': 'hejingying',
+        }
+        self.invalid_signin_invalid_password = {
+            'username': 'hjy@gmail.com',
+            'password': '123123',
+        }
+
+    def test_signin_valid_user(self):
+        client.post(
             reverse('users-list'),
-            data=json.dumps(self.valid_user),
+            data=json.dumps(self.user_previous),
             content_type='application/json'
         )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_add_invalid_user_no_username(self):
         response = client.post(
+            '/auth/',
+            data=json.dumps(self.valid_signin_user),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+    def test_signin_invalid_signin_user_no_username(self):
+        client.post(
             reverse('users-list'),
-            data=json.dumps(self.invalid_user_no_username),
+            data=json.dumps(self.user_previous),
+            content_type='application/json'
+        )
+        response = client.post(
+            '/auth/',
+            data=json.dumps(self.invalid_signin_user_no_username),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('username', response.data)
 
-    def test_add_invalid_user_no_password(self):
-        response = client.post(
+    def test_signin_invalid_signin_user_no_password(self):
+        client.post(
             reverse('users-list'),
-            data=json.dumps(self.invalid_user_no_password),
+            data=json.dumps(self.user_previous),
+            content_type='application/json'
+        )
+        response = client.post(
+            '/auth/',
+            data=json.dumps(self.invalid_signin_user_no_password),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('password', response.data)
 
-    def test_add_invalid_user_no_username_password(self):
-        response = client.post(
+    def test_signin_invalid_signin_nonexist_username(self):
+        client.post(
             reverse('users-list'),
-            data=json.dumps(self.invalid_user_no_username_password),
+            data=json.dumps(self.user_previous),
+            content_type='application/json'
+        )
+        response = client.post(
+            '/auth/',
+            data=json.dumps(self.invalid_signin_nonexist_username),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('username', response.data)
-        self.assertIn('password', response.data)
+        self.assertIn('non_field_errors', response.data)
 
-    def test_add_invalid_user_exist_username(self):
-        user_previous = User.objects.create(
-            username='hjy@gmail.com', password='hejingying123')
-        response = client.post(
+    def test_signin_invalid_signin_invalid_username(self):
+        client.post(
             reverse('users-list'),
-            data=json.dumps(self.invalid_user_exist_username),
+            data=json.dumps(self.user_previous),
+            content_type='application/json'
+        )
+        response = client.post(
+            '/auth/',
+            data=json.dumps(self.invalid_signin_invalid_username),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('username', response.data)
+        self.assertIn('non_field_errors', response.data)
+
+    def test_signin_invalid_signin_wrong_password(self):
+        client.post(
+            reverse('users-list'),
+            data=json.dumps(self.user_previous),
+            content_type='application/json'
+        )
+        response = client.post(
+            '/auth/',
+            data=json.dumps(self.invalid_signin_wrong_password),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('non_field_errors', response.data)
+
+    def test_signin_invalid_signin_invalid_password(self):
+        client.post(
+            reverse('users-list'),
+            data=json.dumps(self.user_previous),
+            content_type='application/json'
+        )
+        response = client.post(
+            '/auth/',
+            data=json.dumps(self.invalid_signin_invalid_password),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('non_field_errors', response.data)
