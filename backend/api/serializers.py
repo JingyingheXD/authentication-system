4c3da167704c8352554e89e2aa5860b36db543fd
Jingying He <jingyingh@student.unimelb.edu.au>
2021-11-28 01:03:39 +1100

    number of movies

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -37,7 +37,7 @@ class UserSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'no_of_ratings')
 
 
 class RatingSerializer(serializers.ModelSerializer):
