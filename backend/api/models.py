594e9a6c15842b8361bea97f3834d44b91be746c
Jingying He <jingyingh@student.unimelb.edu.au>
2021-11-28 01:06:36 +1100

    average ratings

--- backend/api/models.py
+++ backend/api/models.py
@@ -12,6 +12,16 @@ class Movie(models.Model):
         ratings = Rating.objects.filter(movie=self)
         return len(ratings)
 
+    def avg_ratings(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating
+        if len(ratings) > 0:
+            return sum/len(ratings)
+        else:
+            return 0
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -37,7 +37,7 @@ class UserSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'description', 'no_of_ratings')
+        fields = ('id', 'title', 'description', 'no_of_ratings', 'avg_ratings')
 
 
 class RatingSerializer(serializers.ModelSerializer):
